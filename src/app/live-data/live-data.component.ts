9ffef7452d95c8e2ebc774a87278b70eaa3a33ca
namrata2612 <51783686+namrata2612@users.noreply.github.com>
2020-05-13 12:41:30 -0700

    livedataPage-new

--- src/app/live-data/live-data.component.ts
+++ src/app/live-data/live-data.component.ts
@@ -13,47 +13,33 @@ import { Timestamp } from 'rxjs';
   templateUrl: './live-data.component.html',
   styleUrls: ['./live-data.component.css']
 })
+
 export class LiveDataComponent implements OnInit {
 @ViewChild(MatSort) sort: MatSort;
 liveData:any=[]
-<<<<<<< HEAD
 dataSource:any
 loginData:any
-displayedColumns: string[] = ['Sl_No','Contact_1', 'Contact_2', 'Time'];
-=======
-dataSource:any=[]
-loginData:any
 displayedColumns: string[] = ['i','baseName', 'contactName', 'updatedOn'];
->>>>>>> 3c5160d70b815049642b25c0ec765c3867808df3
+
 
   constructor(
     private api: ApiService,
     private login:LoginCheckService,
     private router:Router
   ) {
-<<<<<<< HEAD
    
     console.log(this.sort)
  
    }
-=======
-   }
-
->>>>>>> 3c5160d70b815049642b25c0ec765c3867808df3
 
   ngOnInit(): void {
     this.loginData = this.login.Getlogin()
     this.loginData = JSON.parse(this.loginData)
 
     this.refreshData()
-<<<<<<< HEAD
-  
-    
-=======
     this.dataSource.sort=this.sort
->>>>>>> 3c5160d70b815049642b25c0ec765c3867808df3
+    
   }
-
 
   refreshData(){
     var data={
@@ -65,24 +51,17 @@ displayedColumns: string[] = ['i','baseName', 'contactName', 'updatedOn'];
       console.log("live data ======",res);
       if(res.status){
         this.liveData=res.success
-<<<<<<< HEAD
 
         this.dataSource = new MatTableDataSource(this.liveData);
-        this.dataSource.sort = this.sort;
-         console.log(this.dataSource)
-=======
-        this.dataSource = new MatTableDataSource(this.liveData);
-
         setTimeout(() => {
           this.dataSource.sort = this.sort;
         })
->>>>>>> 3c5160d70b815049642b25c0ec765c3867808df3
+      }
+    })
       }
     })
    
   }
-
-
 
 }
 
