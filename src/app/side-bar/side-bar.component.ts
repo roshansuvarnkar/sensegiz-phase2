f206fd558ac50d349ba94254d3fd8aef91cc8558
namrata2612 <51783686+namrata2612@users.noreply.github.com>
2020-05-27 12:44:57 -0700

    mobile optimization

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 import { LoginCheckService } from '../login-check.service';
-
+import {MatMenuTrigger} from '@angular/material/menu';
+import {MatTableDataSource} from '@angular/material/table';
+import { Timestamp } from 'rxjs';
+import {MatPaginator} from '@angular/material/paginator';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   loginData:any
   findData:any=[]
   checkUrl:any
+  dataSource:any
+  displayedColumns: string[] = ['icon','deviceName'];
 
   constructor(private api: ApiService,private login:LoginCheckService,private router:Router) { }
 
@@ -21,10 +27,11 @@ export class SideBarComponent implements OnInit {
     this.loginData = JSON.parse(this.loginData)
     this.refreshFinds()
     //this.checkPage()
-
+  
+  
   }
 
-
+ 
   refreshFinds(){
     var data={
       userId:this.loginData.userId,
@@ -33,8 +40,19 @@ export class SideBarComponent implements OnInit {
     this.api.getAssignedDevices(data).then((res:any)=>{
       console.log("find data side bar ======",res);
       if(res.status){
-        this.findData=res.success
+      for(let i=0;i<res.success.length;i+1){
+        this.findData.push({
+          icon:'label_important',
+          deviceName:res.success[i].deviceName
+        })
       }
+      }
+      
+      this.dataSource = new MatTableDataSource(this.findData);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+  
+      });
     })
   }
 
@@ -46,8 +64,12 @@ checkPage(){
 }
 
   clickDevice(data){
+    
     console.log("data====",data)
     this.router.navigate(['/device-history'], { queryParams: { record: JSON.stringify(data) } });
   }
 
+  
+
+  
 }
