d72de20216f889569704b89905ab8e4a434e8d46
namratha <namrathanm2612@gmail.com>
2020-11-07 12:05:28 +0530

    proximity/wearable

--- src/app/admin-settings/admin-settings.component.ts
+++ src/app/admin-settings/admin-settings.component.ts
@@ -47,7 +47,7 @@ export class AdminSettingsComponent implements OnInit {
     });
     this.distanceForm = this.fb.group({
       distance: ['', Validators.required],
-      rssi: [''],
+      rssi: ['',Validators.pattern(/^[A-z 0-9]{2}$/)],
       wearable:['',Validators.required],
       customize:['']
 
