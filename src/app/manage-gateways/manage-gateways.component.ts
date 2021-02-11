218a2b319e9aaa9f63efd6e65bd3d6bcf708aa7e
RAJESHKUMARVANKUDOTHU <68320301+RAJESHKUMARVANKUDOTHU@users.noreply.github.com>
2021-02-11 10:31:09 +0530

    changed

--- src/app/manage-gateways/manage-gateways.component.ts
+++ src/app/manage-gateways/manage-gateways.component.ts
@@ -159,6 +159,7 @@ GatewaypgiAlret(value){
       'width':'31px',
         'color':'green',
         'position':'absolute',
+        'background-color':'green',
         'margin-left':'-33px',
         'margin-top':'-2px',
     }
@@ -169,6 +170,7 @@ GatewaypgiAlret(value){
       'width':'18px',
       'color':'red',
       'position':'absolute',
+      'background-color':'red',
       'margin-left':'-30px',
       'margin-top':'-2px',
     }
