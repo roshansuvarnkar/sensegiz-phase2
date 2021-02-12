d26ffaffb76c92e063b4673b4017dd242ed8f357
RAJESHKUMARVANKUDOTHU <68320301+RAJESHKUMARVANKUDOTHU@users.noreply.github.com>
2021-02-12 16:23:11 +0530

    changed

--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -48,7 +48,7 @@ export class WebsocketService {
       // if(data.userName == )
     });
   }
-  
+
   joinRoom(){
     let data = this.login.loginData();
     console.log("data login===",data);
@@ -57,7 +57,7 @@ export class WebsocketService {
 
   leaveRoom(data){
     console.log("leave room emit==",data);
-    
+
     this.socket.emit("leaveRoom",data);
   }
 
