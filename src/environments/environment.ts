1e7a18bbfd6db85338a6fab0a1bfbf61f1c56bd9
RAJESHKUMARVANKUDOTHU <68320301+RAJESHKUMARVANKUDOTHU@users.noreply.github.com>
2021-03-05 10:39:53 +0530

    changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,9 +11,11 @@ apiHost:'https://testdba.sensegiz.com:3000',
     //  apiHost:'http://3.6.7.251:3000',
   // apiHost:'http://65.0.60.192:3000',
 
- // apiHost:'http://65.1.126.197:3000',
-// socketHost:'http://65.1.126.197:3010',
-  socketHost:'https://testdba.sensegiz.com:3010',
+ //apiHost:'http://65.1.126.197:3000',
+ //socketHost:'http://65.1.126.197:3010',
+ socketHost:'https://testdba.sensegiz.com:3010',
+ //apiHost:'http://15.207.101.93:3000',
+ //socketHost:'http://15.207.101.93:3010',
   ENCRYPTKEY:"KeYiSGDQdtgUbfu7LpHpGY8G4VzCczMG",
   production: false
 }
